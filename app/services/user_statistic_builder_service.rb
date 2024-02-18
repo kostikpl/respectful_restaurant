3432d571b21dd48eaaf55044061ea4644c1cf6dc
Konstantin Plyusnin <kostik.plyusnin@gmail.com>
2024-02-19 00:01:55 +0100

    minor

--- app/services/user_statistic_builder_service.rb
+++ app/services/user_statistic_builder_service.rb
@@ -2,6 +2,7 @@ class UserStatisticBuilderService < BaseService
   def initialize(params:)
     @reservation_id = params[:reservation_id]
     @left_at = DateTime.now
+    @customer_id = params[:customer_id]
   end
 
   def call
